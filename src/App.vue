a7a092a8eb393f62af595b0d86d79e2a5797c55d
Kenneth Løvgren <Kenneth.l.thomsen@gmail.com>
2023-03-06 10:45:16 +0100

    example on toref refs

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div class='app'>
     <h1>Hyrule Jobs</h1>
-    <p>Hello, {{ name }}</p>
+    <p>Hello, {{ name }} - {{age}}</p>
+    <button @click="changeName('Zelda')">Change Name</button>
+    <button @click="changeAge('34')">Change age</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+
+
 
 export default defineComponent({
   name: 'App',
   components: {},
-  data () {
-    return {
-      name: 'Link'
-    }
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+
+    // return { ...toRefs(state) }
+
+    const name = ref('Link')
+    const age = ref(<number | number>(25))
+
+    return {name, age}
+
   },
   methods: {
     changeName(name: string) {
       this.name = name
+    },
+    changeAge(age: number | string)  {
+      this.age = age
     }
   }
 })
